ede6ae8063a77c268e41add6c499cba0c1e9be92
unknown <emmansairun@gmail.com>
2023-07-28 22:37:45 +0100

    done with the fuctionality of the app

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,12 @@ import React from 'react';
 function Button({ value, onSquareClick }) {
   return (
     <div>
-      <button className="square" type="button" onClick={onSquareClick}>
+      <button
+        className="square"
+        type="button"
+        style={{ minHeight: '2rem', minWidth: '2rem' }}
+        onClick={onSquareClick}
+      >
         {value}
       </button>
     </div>
